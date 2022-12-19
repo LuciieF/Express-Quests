168ca99d53612b7a87a8f876ec038eb8d2492f35
LuciieF <lucie.froissart5917@gmail.com>
2022-12-19 15:47:27 +0100

    quest 5

--- usersHandlers.js
+++ usersHandlers.js
@@ -53,9 +53,27 @@ const updateUser = (req, res) => {
 
   database
     .query(
-      "update movies set (firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?)",
+      "update users set (firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?)",
       [firstname, lastname, email, city, language]
     )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Can't find any user");
+    });
+};
+
+const deleteUser = (req, res) => {
+  const id = parseInt(req.params.id);
+
+  database
+    .query("delete from users where id = ?", [id])
     .then(([result]) => {
       if (result.affectedRows === 0) {
         res.status(404).send("Not Found");
@@ -74,4 +92,5 @@ module.exports = {
   getUsersById,
   postUser,
   updateUser,
+  deleteUser,
 };
