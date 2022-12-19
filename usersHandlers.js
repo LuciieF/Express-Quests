8dbb2ce1d88ffbe77a757d6d6eff1b226194936e
LuciieF <lucie.froissart5917@gmail.com>
2022-12-19 14:12:34 +0100

    quest 4

--- usersHandlers.js
+++ usersHandlers.js
@@ -47,8 +47,31 @@ const postUser = (req, res) => {
     });
 };
 
+const updateUser = (req, res) => {
+  const id = parseInt(req.params.id);
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "update movies set (firstname = ?, lastname = ?, email = ?, city = ?, language = ? where id = ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.status(404).send("Not Found");
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Can't find any user");
+    });
+};
+
 module.exports = {
   getUsers,
   getUsersById,
   postUser,
+  updateUser,
 };
